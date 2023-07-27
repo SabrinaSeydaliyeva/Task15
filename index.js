4270bba4ba6bc941a5a97ae4ecc9a920beba57bc
SabrinaSeydaliyeva <seydaliyeva.sabrina@bk.ru>
2023-07-27 13:30:39 +0400

    .

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let products=['alma', 'armud', 'heyva']
-let productsCopy=products
-products.push='nar'
-console.log(productsCopy);
+let fruits=['alma', 'armud', 'heyva']
+let fruitsCopy=fruits
+fruits.push='nar'
+console.log(fruitsCopy);
